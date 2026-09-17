6b1432195c38ca8071fea81e4190810a580000ee
Morui <Kirik9481@gmail.com>
2026-09-17 15:43:15 +0300

    Add files via upload

--- assets/js/site.js
+++ assets/js/site.js
@@ -8,16 +8,18 @@
   let mx = 0.5;
   let my = 0.5;
 
+  const update = () => {
+    root.style.setProperty('--mouse-x', `${(mx - 0.5) * 18}px`);
+    root.style.setProperty('--mouse-y', `${(my - 0.5) * 12}px`);
+    raf = 0;
+  };
+
   window.addEventListener('pointermove', (event) => {
+    if (event.pointerType && event.pointerType !== 'mouse') return;
+
     mx = event.clientX / window.innerWidth;
     my = event.clientY / window.innerHeight;
 
-    if (!raf) {
-      raf = requestAnimationFrame(() => {
-        root.style.setProperty('--mouse-x', `${(mx - 0.5) * 18}px`);
-        root.style.setProperty('--mouse-y', `${(my - 0.5) * 12}px`);
-        raf = 0;
-      });
-    }
+    if (!raf) raf = requestAnimationFrame(update);
   }, { passive: true });
 })();
